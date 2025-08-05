a6ee8c5c9936106201f569c646fb5b5aa4428a54
kyrylomalyi81 <kyrylomalyi81@gmail.com>
2025-08-06 00:53:55 +0200

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/kyrylomalyi/rawmileage/RawCameraMileageApplication.java
@@ -0,0 +1,13 @@
+package com.kyrylomalyi.rawmileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawCameraMileageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RawCameraMileageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kyrylomalyi/rawmileage/RawCameraMileageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyrylomalyi.rawmileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RawCameraMileageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
